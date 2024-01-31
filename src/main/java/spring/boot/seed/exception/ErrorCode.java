3e5653c984c2052f5320216091a529533fa05ce6
PhiloMonx1 <philomonx1@gmail.com>
2024-01-31 20:10:36 +0900

    🛠️Refactor : INVALID_PASSWORD 에러코드 HttpStatus 변경 (UNAUTHORIZED -> CONFLICT)

--- src/main/java/spring/boot/seed/exception/ErrorCode.java
+++ src/main/java/spring/boot/seed/exception/ErrorCode.java
@@ -19,7 +19,7 @@ public enum ErrorCode {
 	 */
 	MEMBER_NAME_DUPLICATED(HttpStatus.CONFLICT, "이미 존재하는 memberName 입니다."),
 	MEMBER_NAME_NOT_FOUND(HttpStatus.NOT_FOUND, "찾을 수 없는 memberName 입니다."),
-	INVALID_PASSWORD(HttpStatus.UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
+	INVALID_PASSWORD(HttpStatus.CONFLICT, "비밀번호가 일치하지 않습니다."),
 
 	;
 
