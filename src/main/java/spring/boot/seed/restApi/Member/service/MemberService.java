d4cb3a664361de9cf467fda8c129def276d8f88a
PhiloMonx1 <philomonx1@gmail.com>
2024-01-31 20:34:36 +0900

    🛠️Refactor : 회원가입, 로그인 API 리턴 값 리펙토링 ( 1. 토큰 Header 노출 추가 2. ResponseDto로 매핑하여 리턴 )

--- src/main/java/spring/boot/seed/restApi/Member/service/MemberService.java
+++ src/main/java/spring/boot/seed/restApi/Member/service/MemberService.java
@@ -1,5 +1,6 @@
 package spring.boot.seed.restApi.Member.service;
 
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -7,6 +8,8 @@ import spring.boot.seed.exception.AppException;
 import spring.boot.seed.exception.ErrorCode;
 import spring.boot.seed.restApi.Member.dto.request.MemberJoinRequestDto;
 import spring.boot.seed.restApi.Member.dto.request.MemberLoginRequestDto;
+import spring.boot.seed.restApi.Member.dto.response.MemberResponseDto;
+import spring.boot.seed.restApi.Member.dto.response.MemberTokenResponseDto;
 import spring.boot.seed.restApi.Member.model.MemberEntity;
 import spring.boot.seed.restApi.Member.repository.MemberRepository;
 import spring.boot.seed.utils.JwtUtil;
@@ -18,26 +21,58 @@ public class MemberService {
 	private final MemberRepository memberRepository;
 	private final BCryptPasswordEncoder encoder;
 
-	public String join(MemberJoinRequestDto dto){
+	public MemberTokenResponseDto join(MemberJoinRequestDto dto, HttpServletResponse response) {
 		memberRepository.findByMemberName(dto.getMemberName())
-				.ifPresent(memberEntity -> {throw new AppException(ErrorCode.MEMBER_NAME_DUPLICATED, ErrorCode.MEMBER_NAME_DUPLICATED.getMessage());});
+				.ifPresent(memberEntity -> {
+					throw new AppException(ErrorCode.MEMBER_NAME_DUPLICATED, ErrorCode.MEMBER_NAME_DUPLICATED.getMessage());
+				});
 
-		memberRepository.save(MemberEntity.builder()
+		MemberEntity member = MemberEntity.builder()
 				.memberName(dto.getMemberName())
 				.password(encoder.encode(dto.getPassword()))
-				.build());
+				.build();
 
-		return  "SUCCESS";
+		memberRepository.save(member);
+
+		MemberResponseDto memberResponseDto = MemberResponseDto.builder()
+				.memberId(member.getMemberId())
+				.memberName(member.getMemberName())
+				.createdAt(member.getCreatedAt())
+				.modifiedAt(member.getModifiedAt())
+				.build();
+
+		String accessToken = JwtUtil.createToken(member.getMemberName());
+
+		response.setHeader("accessToken", accessToken);
+
+		return MemberTokenResponseDto.builder()
+				.memberInfo(memberResponseDto)
+				.accessToken(accessToken)
+				.build();
 	}
 
-	public String login(MemberLoginRequestDto dto) {
+	public MemberTokenResponseDto login(MemberLoginRequestDto dto, HttpServletResponse response) {
 		MemberEntity selectedMember = memberRepository.findByMemberName(dto.getMemberName())
-				.orElseThrow(() ->new AppException(ErrorCode.MEMBER_NAME_NOT_FOUND, ErrorCode.MEMBER_NAME_NOT_FOUND.getMessage()));
+				.orElseThrow(() -> new AppException(ErrorCode.MEMBER_NAME_NOT_FOUND, ErrorCode.MEMBER_NAME_NOT_FOUND.getMessage()));
 
-		if(!encoder.matches(dto.getPassword(), selectedMember.getPassword())){
+		if (!encoder.matches(dto.getPassword(), selectedMember.getPassword())) {
 			throw new AppException(ErrorCode.INVALID_PASSWORD, ErrorCode.INVALID_PASSWORD.getMessage());
 		}
 
-		return JwtUtil.createToken(selectedMember.getMemberName());
+		MemberResponseDto memberResponseDto = MemberResponseDto.builder()
+				.memberId(selectedMember.getMemberId())
+				.memberName(selectedMember.getMemberName())
+				.createdAt(selectedMember.getCreatedAt())
+				.modifiedAt(selectedMember.getModifiedAt())
+				.build();
+
+		String accessToken = JwtUtil.createToken(selectedMember.getMemberName());
+
+		response.setHeader("accessToken", accessToken);
+
+		return MemberTokenResponseDto.builder()
+				.memberInfo(memberResponseDto)
+				.accessToken(accessToken)
+				.build();
 	}
 }
